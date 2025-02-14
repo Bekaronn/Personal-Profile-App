bb536a3ebe3b07046b0a37ac2518465fd0d5c6f5
Bekaronn <117936616+Bekaronn@users.noreply.github.com>
2025-02-14 22:37:11 +0500

    first commit

---
+++ Personal Profile App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personal Profile App
+//
+//  Created by Bekarys on 14.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
